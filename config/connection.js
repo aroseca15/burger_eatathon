6e8340ac1bc2cfca03c1f90999dc2c066e0d5899
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2020-11-24 21:23:35 -0700

    Previous repo malfunctioned. New repo made from past work.

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let exphbs  = require('express-handlebars');
+ 
+let app = express();
+ 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/views'));
+ 
+app.get('/', function (req, res) {
+    res.render('home');
+});
+ 
+app.listen(3000);
