a362a15ed31941bac93b4d06b003513a163a55b1
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2020-11-28 15:10:51 -0700

    tried changing variable names in burger.js. Not Working Yet.

--- models/burger.js
+++ models/burger.js
@@ -4,29 +4,29 @@ let Burgers = {
     // Calling all of the ORMs
 
     // Seleting All:
-    SelectALL: async function(res) {
-        let res = await objRelMap.selectAll("burgers");
-        return res;
+    SelectALL: async function(SAres) {
+        let SAres = await objRelMap.selectAll("burgers");
+        return SAres;
     },
 
     // Create:
-    Create: async function(col, val, res) {
-        let res = await objRelMap.create("burgers", col, val);
-        return res;
+    Create: async function(col, val, Cres) {
+        let Cres = await objRelMap.create("burgers", col, val);
+        return Cres;
     },
 
 
     // Delete:
-    Delete: async function(condition, res) {
-        let res = await objRelMap.delete("burgers", condition);
-        return res;
+    Delete: async function(condition, Dres) {
+        let Dres = await objRelMap.delete("burgers", condition);
+        return Dres;
     },
 
 
     // Update:
-    Update: async function(colObjVal, condition, res) {
-        let res = await objRelMap.update("burgers", colObjVal, condition);
-        return res;
+    Update: async function(colObjVal, condition, Ures) {
+        let Ures = await objRelMap.update("burgers", colObjVal, condition);
+        return Ures;
     }
 };
 
