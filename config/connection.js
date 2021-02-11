1fa4f856b1984d2e200450db6cb67d08325c3227
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2021-02-11 16:47:12 -0700

    changed to root for deployment

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ let mysql = require('mysql');
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'ssTm@1722',
+    password: 'root',
     database: 'burgers_Db'
 });
 
